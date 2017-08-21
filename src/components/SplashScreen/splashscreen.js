340f5665d54c1236c7e498aac321844d64e93de0
Vikram Viswanathan <vikram3103@gmail.com>
2017-08-21 20:40:33 +0530

    Splash Screen added

---
+++ src/components/SplashScreen/splashscreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class splashscreen extends Component {
+    render() {
+        return (
+            <View>
+              <Text>Welcome to Marine Insurance!</Text>
+              <Text>Powered by RapidQube Digital Solutions Pvt. Ltd. (TM)</Text>
+            </View>
+        );
+    }
+}
